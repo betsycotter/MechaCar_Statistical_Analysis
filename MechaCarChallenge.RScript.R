2764078892feaf4d3072420bb716585a21d98a34
Betsy <116031639+betsycotter@users.noreply.github.com>
2023-02-15 13:42:22 -0700

    Updated with part 3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -11,3 +11,13 @@ Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFa
 library(tidyverse)
 total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+t.test(Suspension_Coil$PSI,mu=1500)
+
+Lot1_PSI <- subset(Suspension_Coil,Manufacturing_Lot=="Lot1")
+Lot2_PSI <- subset(Suspension_Coil,Manufacturing_Lot=="Lot2")
+Lot3_PSI <- subset(Suspension_Coil,Manufacturing_Lot=="Lot3")
+
+t.test(Lot1_PSI$PSI,mu=1500)
+t.test(Lot2_PSI$PSI,mu=1500)
+t.test(Lot3_PSI$PSI,mu=1500)
